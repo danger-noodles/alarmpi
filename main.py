85f30d8396346697be83fc7b1c313312b61e4522
BIGduzy <nick-b@live.nl>
2016-11-03 19:37:41 +0100

    Code cleanup

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ import time
 
 
 ###############
-## Constands ##
+## Constants ##
 ###############
 # LEDs
 LED_RED = 25
@@ -59,11 +59,20 @@ DIGITS = (4, 22, 10, 11)
 ###############
 ## Functions ##
 ###############
-def ask_pin_code():
+# aks_and_validate_pin_code
+# Aks pin code using the input method and the return if the pin code is correct
+#
+# @return Bool || If pin code is correct or not
+def ask_and_validate_pin_code():
     pin = input('Pin please: ')
     return int(pin) == PIN_CODE
 
 
+# display_seg
+# Displays string on the 7 segment 4 digit display
+#
+# @param string display_string || The string that needs to be displayed, only char in the CHARACTERS constant allowed
+# @return void
 def display_seg(display_string):
     for digit in range(4):
         GPIO.output(SEGMENTS, (CHARACTERS[display_string[digit]]))
@@ -72,10 +81,15 @@ def display_seg(display_string):
         GPIO.output(DIGITS[digit], 1)
 
 
+# blink_alarm
+# Blinks alarm, 7 segment 4 digit display and activates speaker
+# This function is used a a secondary thread
+#
+# @return void
 def blink_alarm():
     print('Alarm Thread')
 
-    # Gobals
+    # Globals
     global g_alarm_on
     global g_alarm_delay
 
@@ -83,31 +97,35 @@ def blink_alarm():
     alarm_time = time.time()
     alarm_on = False
 
+    # Speaker
+    speaker_wait_time = 1.0 / 2000 / 2
+
     while g_alarm_on:
         # blink timer
+        # if current time - old time >= alarm delay
         if time.time() - alarm_time >= g_alarm_delay:
-            alarm_time = time.time()
-            alarm_on = not alarm_on
+            alarm_time = time.time() # reset timer
+            alarm_on = not alarm_on # invert alarm bool
 
         if alarm_on:
+            # Display Boef on 7 segment 4 digit display
             display_seg('BOEF')
-        else:
-            display_seg('    ')
 
-        # NOTE: This does not work 100%
-        if alarm_on:
             # Make speaker bleep
             GPIO.output(SPEAKER_PIN, True)
-            time.sleep(1.0 / 2000 / 2) # TODO: replace this with something else
+            time.sleep(speaker_wait_time)
             GPIO.output(SPEAKER_PIN, False)
-            time.sleep(1.0 / 2000 / 2) # TODO: replace this with something else
+            time.sleep(speaker_wait_time)
+        else:
+            display_seg('    ')
 
         # Alarm LED
         GPIO.output(LED_RED, alarm_on)
 
     GPIO.output(LED_RED, False) # Turn Light off when thread ends
-    display_seg('----')
+    display_seg('    ') # Clear display
     print('Alarm Thread killed')
+
 
 def main():
     # globals
@@ -124,7 +142,7 @@ def main():
         triggered = 3
         settings = 4
 
-    state = State.triggered
+    state = State.off # Default (starting) state is off
 
     # buttons
     button_a_pressed = False
@@ -135,11 +153,18 @@ def main():
     # loop bool
     running = True
 
+    # MAIN LOOP
     while running:
         led_green_on = False
         led_yellow_on = False
 
         # Button input
+        # Check if the button is pressed and set button_a_pressed if it is
+        # We do this so we can check if the button is released and the action is not fired multiple times if
+        # the button is not released
+        # We use This logic for every button
+
+        # Button A
         if GPIO.input(BUTTON_A):
             if not button_a_pressed:
                 print("Button A")
@@ -152,11 +177,11 @@ def main():
         else:
             button_a_pressed = False
 
-        # B
+        # Button B
         if GPIO.input(BUTTON_B):
             if not button_b_pressed:
                 print("Button B")
-                if ask_pin_code():
+                if ask_and_validate_pin_code():
                     # Set alarm on standby
                     if state == State.off:
                         state = State.standby
@@ -174,7 +199,7 @@ def main():
         else:
             button_b_pressed = False
 
-        # C
+        # Button C
         if GPIO.input(BUTTON_C):
             if not button_c_pressed:
                 print("Button C")
@@ -191,18 +216,19 @@ def main():
         else:
             button_c_pressed = False
 
-        # D
+        # Button D
         if GPIO.input(BUTTON_D):
             if not button_d_pressed:
                 print("Button D")
                 # Change blink speed
                 if state == State.settings:
-                    # swap between 1, 2 and 3 delay
+                    # swap between 1, 2 and 3 second delay
                     g_alarm_delay = g_alarm_delay + 1 if g_alarm_delay < 3 else 1
                     print("Alarm delay:", g_alarm_delay)
                 # Turn off program (can't turn on again)
                 elif state == State.off:
                     running = False
+                    
             button_d_pressed = True
         else:
             button_d_pressed = False
@@ -228,8 +254,9 @@ def main():
             g_alarm_on = True # Turn on second thread for alarm
             led_green_on = True
 
-
+        # Do not create thread if the old one is still alive
         if g_alarm_on and not alarm_thread.isAlive():
+            # We can't reuse the old thread so we need to create a new one
             alarm_thread = threading.Thread(target = blink_alarm)
             alarm_thread.daemon = True # Tread dies when program ends
             alarm_thread.start()
@@ -238,16 +265,19 @@ def main():
         GPIO.output(LED_GREEN, led_green_on)
         GPIO.output(LED_YELLOW, led_yellow_on)
 
+
 ###############
 ##    Init   ##
 ###############
+# Set the GPIO mode
 GPIO.setmode(GPIO.BCM)
+# Turn of warnings to keep the console clean
 GPIO.setwarnings(False)
 
 # LEDS
-GPIO.setup(LED_RED, GPIO.OUT)
-GPIO.setup(LED_YELLOW, GPIO.OUT)
-GPIO.setup(LED_GREEN, GPIO.OUT)
+GPIO.setup(LED_RED, GPIO.OUT, initial = False)
+GPIO.setup(LED_YELLOW, GPIO.OUT, initial = False)
+GPIO.setup(LED_GREEN, GPIO.OUT, initial = False)
 
 # BUTTONS
 GPIO.setup(BUTTON_A, GPIO.IN, pull_up_down = GPIO.PUD_UP)
@@ -258,16 +288,8 @@ GPIO.setup(BUTTON_D, GPIO.IN, pull_up_down = GPIO.PUD_UP)
 # SPEAKER
 GPIO.setup(SPEAKER_PIN, GPIO.OUT)
 
-# Turn lights of by default
-GPIO.output(LED_RED, False)
-GPIO.output(LED_YELLOW, False)
-GPIO.output(LED_GREEN, False)
-
 # DIGIT DISPLAY
-# 7seg_segment_pins (11,7,4,2,1,10,5,3)
 GPIO.setup(SEGMENTS, GPIO.OUT, initial = 0)
-
-# 7seg_digit_pins (12,9,8,6) digits 0-3 respectively
 GPIO.setup(DIGITS, GPIO.OUT, initial = 1)
 
 
@@ -284,5 +306,5 @@ g_alarm_delay = 1
 ###############
 main()
 
-# Turn every ting off when program closes
+# Turn everything off when program closes
 GPIO.cleanup()
