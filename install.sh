ffa0d193af9b5e935cee7aed268e6468fc03f71d
onodera <onodera@openmailbox.org>
2016-11-04 13:42:02 +0100

    Add install script

---
+++ install.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+if [[ $(id -u) != 0 ]]; then
+	echo 'Please run this script as root!'
+	exit 1
+fi
+
+echo 'Cloning github.com/danger-noodles/alarmpi...'
+git clone -q git://github.com/danger-noodles/alarmpi /tmp/alarmpi
+
+echo 'Installing alarmpi script to $PATH...'
+install -Dm755 /tmp/alarmpi/main.py /usr/bin/alarmpi
+
+echo 'Installing alarmpi systemd service file...'
+install -Dm755 /tmp/alarmpi/main.service /lib/systemd/system/alarmpi.service
