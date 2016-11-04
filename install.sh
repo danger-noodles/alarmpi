8435cce14cf277290fb450f10862a78f3f5d93b2
onodera <onodera@openmailbox.org>
2016-11-04 14:36:55 +0100

    Pull if the git is already cloned

--- install.sh
+++ install.sh
@@ -6,7 +6,12 @@ if [[ $(id -u) != 0 ]]; then
 fi
 
 echo 'Cloning github.com/danger-noodles/alarmpi...'
-git clone -q git://github.com/danger-noodles/alarmpi /tmp/alarmpi
+if cd /tmp/alarmpi; then
+	git fetch -q; git reset --hard -q origin/master
+else
+	git clone -q git://github.com/danger-noodles/alarmpi /tmp/alarmpi
+	cd $name
+fi
 
 echo 'Installing alarmpi script to $PATH...'
 install -Dm755 /tmp/alarmpi/main.py /usr/bin/alarmpi
